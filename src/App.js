b5942743881588e02fddd6c92e1cc5750f76318a
Decodedal <Palumbodallas@gmail.com>
2022-09-03 01:12:57 -0400

    app is working

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@
 import './App.css';
 import React, {useState} from 'react'
 import ColorBlock from './colorBlock';
+import ColorForm from './ColorForm';
 
 function App() {
-  let colors = ['violet','blue','lightblue','green','greenyellow','yellow','orange','red']
+  let [colors, setColors] = useState(['violet','blue','lightblue','green','greenyellow','yellow','orange','red'])
   let colorMap = colors.map((color,i)=>{
-   return <ColorBlock color={color}/>
+   return <ColorBlock key={i} color={color}/>
   })
+  const addColor = (newColor) =>{
+    setColors([...colors, newColor])
+  }
   return (
     <div className="App">
     {colorMap}
+    <ColorForm addColor={addColor}/>
     </div>
   )
 }
